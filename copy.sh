a9f38a41371bad1bc03b784bd9b1cb40c4213209
Aleksandr Uvarov <sanyauvarov@mail.ru>
2016-04-25 13:51:18 +0300

    +copy.sh

---
+++ copy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+mv ~/.vim{,-old}
+cp -r .vim ~
+
+mv ~/.bashrc{,-old}
+cp .bashrc ~
+
+mv ~/.git-prompt{,-old}
+cp .git-prompt ~
+
+mv ~/.inputrc{,-old}
+cp .inputrc ~
+
+mv ~/.minttyrc{,-old}
+cp .minttyrc ~
+
+mv ~/.vimrc{,-old}
+cp .vimrc ~
+
